2a6dfe4ad997fb12a6463456537851a3eb5fb5c1
Hodoo08 <hodo08@hotmail.co.uk>
2025-03-07 14:51:33 +0300

    created superuser

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-xrba4)$p%%08gugj9t!+ug8qos#iigcltt$d95tgoc3&y4&la8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
