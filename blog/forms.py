beaaa33912ef89cf5b3ab7f8afc47c9926bfb62e
Hodoo08 <hodo08@hotmail.co.uk>
2025-03-18 17:03:08 +0300

    added succes message for a posted comment

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,8 @@
-from .models import Comment
+from .models import comment
 from django import forms
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = Comment
+        model = comment
         fields = ('body',)
--- blog/models.py
+++ blog/models.py
@@ -4,25 +4,26 @@ from django.contrib.auth.models import User
 STATUS = ((0, 'Draft'), (1, 'Publish'))
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE, 
-        related_name='blog_posts'
+        User, on_delete=models.CASCADE, related_name='blog_posts'
         )
-    class meta:
-        ordering = ['-created_on']
-    def __str__(self):
-        return f"{self.title} | written by {self.author}"
-    
-    
+        
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True) 
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_on']
+        
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 
 class comment(models.Model):
@@ -39,10 +40,11 @@ class comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     
-    
-    class meta:
+    class Meta:
         ordering = ['created_on']
+    
     def __str__(self):
+        return f"comment {self.body} by {self.author}"
         return f"comment {self.body} by {self.author}"
     
     
