a5eea1f2303fbdcc529a3259451fdf2a1a9b18ed
Hodoo08 <hodo08@hotmail.co.uk>
2025-03-18 16:24:25 +0300

    added crispy forms to views

--- blog/models.py
+++ blog/models.py
@@ -24,6 +24,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+
 class comment(models.Model):
     post = models.ForeignKey(
         Post, 
